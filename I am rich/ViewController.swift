6ed687124f31b4309a45fb7733b5b325774dbeb8
Hantz509 <guerrierhantz58@gmail.com>
2024-07-09 13:18:28 +0800

    first commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by GUERRIER Hantz Brunet on 2024/7/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+ 
